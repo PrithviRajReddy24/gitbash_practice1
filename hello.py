d2b0671c4033c0cd034dfd52c8b3daa5958d4285
unknown <velma.pruthvi24@gmail.com>
2021-02-09 23:17:30 +0530

    Edited hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+data = read_csv()
+data.head()
+data.tail()
+data.describe()
+data.info
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn.metrics import accuracy_score,confussion_matrix,f1_precesion_recall_score
